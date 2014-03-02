b40a8fdd8dc4066a0201593172924bd95f6ef4df
Andrew Dolce <andrew.dolce@gmail.com>
2014-03-01 22:10:35 -0500

    Display content inside correct elements

--- js/main.js
+++ js/main.js
@@ -1,22 +1,49 @@
 (function() {
   $(document).ready(function() {
-    $('#main-content div').each(function( index ) {
-      var link = $('.link', this)[0];
-      var linkContent = $('.link', this)[0].innerHTML;
-      var newItem = $("<li>").append(linkContent).click(function () {
-	$('#main-content div').hide();
-	$('#main-content div:nth-child(' + ( index + 1 ) + ')').show();
+
+    var subCategories = {};
+
+    $('#main-content div.category').each(function( index ) {
+      var categoryName = $( this ).attr( 'data-category' );
+
+      var $list = $( '<ul></ul>' );
+
+      $( '> div', this ).each(function( index ) {
+        var link = $( '.link', this )[0];
+        var linkContent = link.innerHTML;
+
+        link.remove();
+        var content = $( this );
+
+        var newItem = $("<li>").append(linkContent).click(function () {
+          $( '#content-container' )
+            .empty()
+            .append( content );
+        });
+
+        $list.append( newItem );
+
+        //link.remove();
       });
-      $('#sidebar ul').append(newItem);
-      link.remove();
+
+      subCategories[ categoryName ] = $list;
     });
+
+    var showSlideContent = function( $slide ) {
+      var categoryName = $slide.attr( 'data-category' );
+      var content = subCategories[ categoryName ];
+      $( '#sub-category' )
+        .empty()
+        .append( content );
+    };
 
     $("#horizAccord").liteAccordion({
       "theme":"custom",
       "containerWidth":"800",
       "containerHeight":"140",
       "headerWidth":"80",
-      "slideSpeed":"400"
+      "slideSpeed":"400",
+      "onTriggerSlide": showSlideContent
     });
   });
 }());
