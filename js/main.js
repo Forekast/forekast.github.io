a39e3959171c64c3c90d688605d32db56bbc31db
Forekast <Forekast@users.noreply.github.com>
2022-05-11 20:12:16 -0400

    Return Folders and Image Files
    ajax call to generate arrays for later use

--- js/main.js
+++ js/main.js
@@ -1,7 +1,55 @@
+$(document).ready(function () {
+    
+    var allImages = "./img/";
+    var foldernames = [];
+    var filenames = [];
+    
+    // Grab images and folder structure
+    $.ajax({
+        url : allImages,
+        success : function (data) {
+            
+            // console.log(data);
+
+            $(data).find('a').attr('href', function (i, val) {
+                // console.log(this);
+                if(val.match(/\.(jpe?g|png|gif)$/)) {
+                    filenames.push($(this).attr('title'));
+                } else if ( val.match( (/(?:\/|^)[^.\/]+$/) )) {
+                    if ($(this).attr('title') != undefined){
+                        foldernames.push($(this).attr('title'));
+                    }
+                }
+            })
+
+        }
+    })
+
+    console.log("Folder Names: ");
+    for (let i = 0; i < foldernames.length; i++){
+        console.log(foldernames[i]);
+    }
+    console.log( "File Names: " + filenames);
+
+
+    // console.log("Document Ready");
+});
+
 function downloadResume() {
-    window.open("docs/2019.09_MakMendelson_Resume.pdf#zoom=200", '_blank');
+  window.open("docs/2019.09_MakMendelson_Resume.pdf#zoom=200", "_blank");
 }
 
-// function calcSpacing() {
-//     $('#post-spacing').
-// }
+
+
+function getDIR(root) {
+    
+}
+
+
+
+
+
+// (/\.(jpe?g|png|gif)$/)
+// (/(?:\/|^)[^.\/]+$/)
+
+// (/^(.+)\/([^\/]+)$/)
